fa7f2766b499f095010e2f9a08c8082d42fd5cdb
Kingsintersect <kingsintersect@gmail.com>
2025-08-28 13:26:20 +0100

    Deployment-8

--- src/app/(application)/admission/components/WelcomeCard.tsx
+++ src/app/(application)/admission/components/WelcomeCard.tsx
@@ -6,8 +6,10 @@ export const WelcomeCard = ({ user }) => {
             <h2 className="text-4xl font-bold text-gray-900 mb-4">Welcome to Your Admission Journey!</h2>
             <p className="text-xl text-gray-600 mb-2">Hello <span className="text-site-a-dark font-bold">{user?.first_name}</span>, you're just a few steps away from joining our university.</p>
             <p className="text-gray-500">Follow the simple process below to complete your admission application.</p>
-            <h5 className='text-base font-bold text-site-a-dark'>You can also login with your Reference  <span className="inline-block p-2 ml-5 rounded-sm bg-pink-100 text-pink-600">{user.reference}</span></h5>
-            <hr className="my-5" />
+            {user?.reference && <>
+                <h5 className='text-base font-bold text-site-a-dark'>You can also login with your Reference  <span className="inline-block p-2 ml-5 rounded-sm bg-pink-100 text-pink-600">{user.reference}</span></h5>
+                <hr className="my-5" />
+            </>}
         </div>
     )
 }
